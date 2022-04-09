c303bf5bbc9086c710fad06b158513120d7cb995
BajkaO <93444998+BajkaO@users.noreply.github.com>
2022-04-09 19:45:19 +0200

    Add files via upload

---
+++ cw2/Brand.java
@@ -0,0 +1,7 @@
+package g13c.cw2.zd;
+
+public enum Brand {
+    Toyota,
+    BMW,
+    Ferrari
+}
---
+++ cw2/Insurance.java
@@ -0,0 +1,42 @@
+package g13c.cw2.zd;
+
+import java.util.List;
+
+public class Insurance {
+
+    private String name;
+    private int number;
+
+    public Insurance(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        //TODO checks
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        //TODO checks
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Insurance{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                '}';
+    }
+
+
+}
